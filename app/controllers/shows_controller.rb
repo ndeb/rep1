b4e1e9aa8382803488b175f0fc25c244930f6141
ndeb <debonnaventure@noe.local>
2014-11-05 17:14:39 +0100

    un message de commit coquin

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,74 +1,99 @@
 class ShowsController < ApplicationController
-  before_action :set_show, only: [:show, :edit, :update, :destroy]
-
-  # GET /shows
-  # GET /shows.json
-  def index
-    @shows = Show.all
-  end
-
-  # GET /shows/1
-  # GET /shows/1.json
-  def show
-  end
-
-  # GET /shows/new
-  def new
-    @show = Show.new
-  end
-
-  # GET /shows/1/edit
-  def edit
-  end
-
-  # POST /shows
-  # POST /shows.json
-  def create
-    @show = Show.new(show_params)
-
-    respond_to do |format|
-      if @show.save
-        format.html { redirect_to @show, notice: 'Show was successfully created.' }
-        format.json { render :show, status: :created, location: @show }
-      else
-        format.html { render :new }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
+    # On ajoute la méthode book dans la liste des méthodes où on set le show au début
+    before_action :set_show, only: [:show, :edit, :update, :destroy, :book]
+    
+    # On saute une etape de securite si on appel BOOK en JSON
+    skip_before_action :verify_authenticity_token, only: [:book]
+    
+    # GET /shows
+    # GET /shows.json
+    def index
+        @shows = Show.all
     end
-  end
-
-  # PATCH/PUT /shows/1
-  # PATCH/PUT /shows/1.json
-  def update
-    respond_to do |format|
-      if @show.update(show_params)
-        format.html { redirect_to @show, notice: 'Show was successfully updated.' }
-        format.json { render :show, status: :ok, location: @show }
-      else
-        format.html { render :edit }
-        format.json { render json: @show.errors, status: :unprocessable_entity }
-      end
+    
+    # GET /shows/1
+    # GET /shows/1.json
+    def show
     end
-  end
-
-  # DELETE /shows/1
-  # DELETE /shows/1.json
-  def destroy
-    @show.destroy
-    respond_to do |format|
-      format.html { redirect_to shows_url, notice: 'Show was successfully destroyed.' }
-      format.json { head :no_content }
+    
+    # GET /shows/new
+    def new
+        @show = Show.new
     end
-  end
-
-  private
+    
+    # GET /shows/1/edit
+    def edit
+    end
+    
+    # POST /shows
+    # POST /shows.json
+    def create
+        @show = Show.new(show_params)
+        
+        respond_to do |format|
+            if @show.save
+                format.html { redirect_to @show, notice: 'Show was successfully created.' }
+                format.json { render :show, status: :created, location: @show }
+                else
+                format.html { render :new }
+                format.json { render json: @show.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+    
+    # PATCH/PUT /shows/1
+    # PATCH/PUT /shows/1.json
+    def update
+        respond_to do |format|
+            if @show.update(show_params)
+                format.html { redirect_to @show, notice: 'Show was successfully updated.' }
+                format.json { render :show, status: :ok, location: @show }
+                else
+                format.html { render :edit }
+                format.json { render json: @show.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+    
+    # DELETE /shows/1
+    # DELETE /shows/1.json
+    def destroy
+        @show.destroy
+        respond_to do |format|
+            format.html { redirect_to shows_url, notice: 'Show was successfully destroyed.' }
+            format.json { head :no_content }
+        end
+    end
+    
+    # POST /shows/1/book.json
+    def book
+        # On crée un nouvel objet booking à partir des paramètres reçus
+        @booking = Booking.new(booking_params)
+        # On précise que cet object Booking dépend du show concerné
+        @booking.show = @show
+        
+        respond_to do |format|
+            if @booking.save
+                format.json
+                else
+                format.json { render json: @booking.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+    
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_show
-      @show = Show.find(params[:id])
+        @show = Show.find(params[:id])
     end
-
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def show_params
-      params.require(:show).permit(:name, :venue, :description, :capacity, :price, :image, :date)
+        params.require(:show).permit(:name, :location, :description, :capacity, :price, :image, :date)
+    end
+    
+    # On ajoute les paramètres qu'on va envoyer avec le booking
+    def booking_params
+        params.require(:booking).permit(:user_name, :seats)
     end
 end
